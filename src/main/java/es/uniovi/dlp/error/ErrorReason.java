bbe45ff58b0bb8c3cf4beaf5c6d2b33697d3243c
Luis Emilio Velasco Sanchez <emibloque@gmail.com>
2021-03-22 16:58:25 +0100

    Add lab 08

--- src/main/java/es/uniovi/dlp/error/ErrorReason.java
+++ src/main/java/es/uniovi/dlp/error/ErrorReason.java
@@ -3,6 +3,10 @@ package es.uniovi.dlp.error;
 public enum ErrorReason {
     LVALUE_REQUIRED("Lvalue required"),
     FIELD_ALREADY_DECLARED("Field already declared in struct"),
+    VARIABLE_ALREADY_DECLARED("Variable already declared"),
+    VARIABLE_NOT_DECLARED("Variable not declared"),
+    FUNCTION_ALREADY_DECLARED("Function already declared"),
+    FUNCTION_NOT_DECLARED("Function not declared"),
     ;
 
     private final String message;
